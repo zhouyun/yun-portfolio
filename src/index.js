0ab9f0366ff729d8553d4591b49e10355e386ea8
kinoko0841 <mengtianshu0841@gmail.com>
2018-06-05 22:26:30 -0700

    build

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
