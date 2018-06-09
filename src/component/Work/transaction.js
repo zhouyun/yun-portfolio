ff832f5577792ece2971f7549834b95eaa74d862
yun zhou <zhouyunsunnyday@gmail.com>
2018-06-08 21:45:50 -0700

    upload content for 3 projects

--- src/component/Work/transaction.js
+++ src/component/Work/transaction.js
@@ -9,122 +9,112 @@ import './css/work.css';
 export class Transaction extends Component {
   render() {
     return (
-      <div className="work">
+      <div className="transaction">
       	<div className="side-bar">
 	      	<Sidebar>
 	      		<div>
-	      			palmdrive website
+	      			Transaction Management Design
 	      		</div>
 	      		<div><div>01</div><Link to="#background">Background</Link></div>
-	      		<div><div>02</div><Link to="#digital-rocks">Digital rocks</Link></div>
-	      		<div><div>03</div><Link to="#design-shape">The design shapes for the content</Link></div>
-	      		<div><div>04</div><Link to="#great-time">Design Shape</Link></div>
-	      		<div><div>05</div><Link to="#to-be-continue">Design Shape</Link></div>
-	      		<div><div>06</div><Link to="#design-team-x">Design Shape</Link></div>
+	      		<div><div>02</div><Link to="#challenge">The challenge</Link></div>
+	      		<div><div>03</div><Link to="#logic-comes">Logic comes the first</Link></div>
+	      		<div><div>04</div><Link to="#design-comes">Design comes the next</Link></div>
+	      		<div><div>05</div><Link to="#into-the-details">Into the details</Link></div>
+	      		<div><div>06</div><Link to="#always-fun">It’s always fun to play with data</Link></div>
+	      		<div><div>07</div><Link to="#hover-is">"Hover" is better than "click and clear"</Link></div>
+	      		<div><div>08</div><Link to="#making-pixels">Making pixels perfect</Link></div>
 	      	</Sidebar>
 	      </div>
-      	<div className="new-year-sprint">
+      	<div className="transaction-management-design">
       		<h3>
-      			New Year Event Sprint
+      			Transaction Management Design
       		</h3>
-      		<span className="december-2016">
-      			December 2016
+      		<span className="June-2014-December-2015">
+      			June 2014 - December 2015
       		</span>
       		<div id={'background'}>
 	          <div className="section-title"> Background </div>
 	        </div>
       		<p>
-      			Different from the traditional international education agency who make profit by providing studying abroad related information, Palmdrive shares it for free. With the belief of helping more Chinese students study abroad, Palmdrive operates a platform for students who want to study abroad with who studied abroad or is studying abroad. 
+      			Perfin is a personal financial software company. It was founded in 2011 and is headquartered in New York.  
+
       		</p>
-      		<p>
-						In order to be known by the market, branding events were launched fast and massively. In this case, events for different marketing preferences were developed. 
-      		</p>
-      		<p>
-						New Year Event was the first city event to offer an opportunity for Palmdrive to communicate with college students and add their belief to our brand.
-      		</p>
+      		
 	        <div className="section-sub-title"> My Role </div>
       		<p>
-      			I collaborated with Su, the event executor, since the start of the project. I delivered digital posters, website, and invitation email for different platforms in 2 weeks.
+      			I worked at Pefin s a UX/UI designer for six months. 
       		</p>
+      		<p>
+      			I was in charge of improving transaction experience by introducing filters and trending data visualization. 
+      		</p>
+      		
           <div className="project-img-div-long">
       			<img
       			className="project-img-long"
 	          src='/image/desktop/newyear-display-1.png'
 	        />
 	        </div>
-      		<div id={'digital-rocks'}>
-	          <div className="section-title" id="digital-rocks"> Digital rocks </div>
+      		<div id={'challenge'}>
+	          <div className="section-title" id="challenge "> The challenge </div>
 	        </div>
       		<p>
-      		With limit budget and time, we decided to cut off the printing part and promote the event only on digital platforms which would reduce the budget by 30% and preparation time by 50%. 
+      		Based on our client’s feedback, costumed tags could be added to transactions and be used as filters to check the total amount. So my first job was to figure out the logic behind the chosen filters.
+
       		</p>
-      		<p>     		
-					Moreover, it was easy for our targets to repost, @ their friends and interact with us on social media platforms, like Wechat. 
-					</p>
-      		<p>
-					As the promotion would happen only on digital, I was so excited to start my design with no worries about the CMYK and print quality. First time I could use some real bold colors!
-					</p>
-      		<p>
-					To be more festive, colorful confetti was added and I finished the key visual in 4 hours. `}
-					</p>
-					<div id={'design-shape'}>
-	          <div className="section-title"> The design shapes for the content </div>
+      		
+					<div id={'logic-comes'}>
+	          <div className="section-title"> Logic comes the first </div>
 	        </div>
 	        <p>
-	        	To get a better promotion effect on different platforms, we customized copies. 
+	        	To make the logic clear, I created two kinds of filter pools. One was for “Must-have” tags, and the other was for “May-have” tags. This allowed users to choose any combination of filters. Later, we found that this system was too complicated for users, so we saved it for advanced settings.
 	        </p>
-	        <p>
-						On Weibo and Wechat Moment, we used 9 images with questions to interact with our followers. 9-image-display forms a whole picture, leading to big visual impact.
-	        </p>
-	        <p>
-	        	After that, we posted image with little poems to express our thanks and sincere invitation on Wechat Public Account. Our followers extracted the QR code to take the survey and sign up for the event.
-	        </p>
-	        <p>
-	        	According to the surveys, we selected the final invitees and sent invitation emails with their name and the party schedule.
-	        </p>
-	        <p>
-	        	To get continuous exposure and a large audience, I also designed Countdown Posters with QR code of the survey.
-	        </p>
-	        <p>
-	        	With all the effort, we got over 200 replies on the first day which was much higher than our expectation according to our previous experience.
-	        </p>
-					<div id={'great-time'}>
-	        	<div className="section-title">They had a great time</div>
+	      
+					<div id={'design-comes'}>
+	        	<div className="section-title">Design comes the next</div>
 	        </div>
 	        <p>
-	        	We borrowed Kumamon custume, which was a very popular character at that time, with the rest budget, and people love it!
+	        	To make the logic clear, I created two kinds of filter pools. One was for “Must-have” tags, and the other was for “May-have” tags. This allowed users to choose any combination of filters. Later, we found that this system was too complicated for users, so we saved it for advanced settings.
 	        </p>
 	        <p>
-	        	In all, our continuous promotion with consistent and vivid design impressed our young participants, offering them an unforgettable experience.
+	        	Based on customer research, we decided to do the transaction user interface as a table list. After studying the precedents, I found most of the table lists were boring and lacking structure, they were a web version of Excel. So I transformed part of the information into a visualization that could be understood immediately so that users could focus on transactions without distraction.
 	        </p>
-	        <p>
-						The event was a big success --
-					</p>
+	        
 
-					<ol>
-						<li>
-							All who registered for the event showed up. The loss rate is 0%, far beyond our expectation.
-						</li>
-						<li>We got 85% feedback rate and 90% were satisfied with the party because they feel they were inspired by other people’s stories.
-						</li>
-						<li>60% posted on their own social media and 92% mentioned Palmdrive.
-						</li>
-						<li>100% showed their interest in next event held by us and left their personal contact for further cooperation.
-						</li>
-					</ol>
-
-	        <div id={'to-be-continue'}>
-	        	<div className="section-title">To be continued</div>
+	        <div id={'into-the-details'}>
+	        	<div className="section-title">Into the details</div>
 	        </div>
 	        <p>
-	        	To make the event official and accessible by larger audience, I created a website for promotion and check in so we could collect the data automatically for later analytics.
+	        	Tags shows below each transaction. Users click a tag to add as a filter and edit filters by clicking bubble to enter a edit mode.
 	        </p>
-	        <p>Two weeks later, we host another New Year event at Sunnyvale.</p>
-	        <div id={'design-team-x'}>
-	        	<div className="section-title">Design Team X Marketing Team</div>
+	        <div className="section-sub-title"> 1. Space is better than lines.</div>
+	         <p>
+	        	I removed the boxes around elements to make a cleaner user interface.
+	        </p> 
+	        <div className="section-sub-title"> 2. Different sizes for different layers.</div>
+	         <p>
+	        	Users could get a clearer picture of what transactions were made each day by viewing the bubble tree.
+	        </p>
+	        <div className="section-sub-title"> 3. Use colors properly.</div>
+	         <p>
+	        	I made a list of colors which complemented each other nicely. Moreover, any color could be told from others easily at the same time. After tags attaching to each transaction, users tended to click tags much more often than typing in a search bar. While people could only added filter by typing in a search bar when using Mint, our competitive product
+	        </p>
+	        <div id={'always-fun'}>
+	        	<div className="section-title">It’s always fun to play with data</div>
 	        </div>
 	        <p>
-	        	Aside the New Year Event, our design team collaborated with marketing team to launch other 136 branding events nationwide, deeply engaging more than 6,000 college students. To promote those events, we created posters, brochures, H5 ads, videos, web ads, web-based games and etc. This directly leads to mainland China sales revenue grew by 233% from 1 to 3.33 million USD in 2 years, increasing market share by 2.5%. 
+	        	The webpages were designed with both bar and pie charts for different needs. 90% users interacted with both pie charts and bar charts. We also had bubble charts or not for a A/B testing, and we removed it for this version.
+	        </p>
+	        <div id={'hover-is'}>
+	        	<div className="section-title">"Hover" is better than "click and clear"</div>
+	        </div>
+	        <p>
+	        	TBased on the researches, I designed with a new feature which allows users to hover on a specific category to see its spending trends instead of clicking it to add a filter. It also saves time of clearing filter pool. In fact, users' interactions with the elements increased by 167% on this page. On iPad, users press instead of hovering.
+	        </p>
+	         <div id={'making-pixels'}>
+	        	<div className="section-title">Making pixels perfect</div>
+	        </div>
+	        <p>
+	        	I did not have the chance to finalize the product, and would love to show you more, however this project is under NDA. What is shown here is only a sneak peek. Please contact me for more details.
 	        </p>
       	</div>
       </div>
