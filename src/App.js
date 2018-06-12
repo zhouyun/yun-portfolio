a6343e763f4d864ec3267564d4c03f7c3d67ec1e
Yaozhong Kang <hellodrizzle@fb.com>
2018-06-11 21:53:32 -0700

    styling for header and some misc

--- src/App.js
+++ src/App.js
@@ -46,7 +46,8 @@ class ScrollTopTop extends Component {
       <div
         onClick={this._handleClick}
         className={`scroll-to-top-root ${this.state.shouldDisplay ? '' : 'hidden_elem'}`}>
-          <img
+        <div className="scroll-to-top-background"/>
+        <img
           className="project-img-long"
           src='/image/desktop/up-1.png'
         />
@@ -54,26 +55,39 @@ class ScrollTopTop extends Component {
     );
   }
 }
-const App = ({history}) => (
-  <div>
-    <Header/>
-    <Router location={history}>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/about/" component={About} />
-        <Route path="/resume/" component={Resume} />
-        <Route path="/work/newyear/" component={Work}/>
-        <Route path="/work/palmdrive/" component={Palmdrive} />
-        <Route path="/work/graphic/" component={GraphicDesign} />
-        <Route path="/work/transaction/" component={Transaction} />
-        <ScrollTopTop/>
-      </div>
-    </Router>
-    <Footer/>
-  </div>
-);
+const App = ({history}) => {
+  return (
+    <div>
+      <Header/>
+      <Router location={history}>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about/" component={About} />
+          <Route path="/resume/" component={Resume} />
+          <Route path="/work/newyear/" component={Work}/>
+          <Route path="/work/palmdrive/" component={Palmdrive} />
+          <Route path="/work/graphic/" component={GraphicDesign} />
+          <Route path="/work/transaction/" component={Transaction} />
+          <ScrollTopTop/>
+        </div>
+      </Router>
+      <Footer/>
+    </div>
+  );
+}
 
 class Home extends Component {
+
+  _renderArrow() {
+    return (
+      <div className="view-case">
+        <div className="view-case-description">View case</div>
+        <div className="view-case-arrow"></div>
+        <div className="view-case-arrow-head">></div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -86,78 +100,69 @@ class Home extends Component {
           <div className="UI-designer-Be">A UX/UI designer. Believe in relationships with people. Think with writing and sketch. Learn by making stuff. Currently located in Bay Area, CA.</div>
         </div>
         <SNSIcon/>
-        <div className="divider">
+        {/* <div ref={this._navPositioner} className="divider">
           <Link to="/work/newyear">-WORK-</Link>
           <Link to="/about">-ABOUT-</Link>
           <Link to="/resume">-RESUME-</Link>
-        </div>
+        </div> */}
         {/*<div className="project-section">
-                  <div className="img-block" src={logo}>
-                  </div>
-                  <div className="text-block">
-                    Fruits Management App
-                  </div>
-                </div>*/}
+                <div className="img-block" src={logo}>
+                </div>
+                <div className="text-block">
+                  Fruits Management App
+                </div>
+              </div>*/}
         <div className="project-section project-new-year-section">
-          <div className="img-block">
-            <img className="background-cloud" src="svg/bluecloud.svg"/>
-            <img className="project-image" src="home/group-2/group-2@2x.png"/>
-
-          </div>
-          <div className="text-block">
-            <div className="project-title">New Year Event Sprint</div>
-            <div className="project-description">A branding event from scratch to the end in 2 weeks</div>
-            <div className="view-case">
-              <Link to="/work/newyear" className="view-case-description">view case</Link>
-              <div className="view-case-arrow"></div>
-              <div className="view-case-arrow-head">></div>
+          <Link to="/work/newyear">
+            <div className="img-block">
+              <img className="background-cloud" src="svg/bluecloud.svg"/>
+              <img className="project-image" src="home/group-2/group-2@2x.png"/>
             </div>
-          </div>
+            <div className="text-block">
+              <div className="project-title">New Year Event Sprint</div>
+              <div className="project-description">A branding event from scratch to the end in 2 weeks</div>
+              {this._renderArrow()}
+            </div>
+          </Link>
         </div>
         <div className="project-section project-palmdrive-section">
-          <div className="img-block" src={logo}>
-            <img className="background-cloud green-cloud" src="svg/greencloud.svg"/>
-            <img className="project-image" src="home/group/group@3x.png"/>
-          </div>
-          <div className="text-block">
-            <div className="project-title">Palmdrive Website</div>
-            <div className="project-description">Official website of Palmdrive</div>
-            <div className="view-case">
-              <Link to="/work/palmdrive" className="view-case-description">view case</Link>
-              <div className="view-case-arrow"></div>
-              <div className="view-case-arrow-head">></div>
+          <Link to="/work/palmdrive">
+            <div className="img-block" src={logo}>
+              <img className="background-cloud green-cloud" src="svg/greencloud.svg"/>
+              <img className="project-image" src="home/group/group@3x.png"/>
             </div>
-          </div>
+            <div className="text-block">
+              <div className="project-title">Palmdrive Website</div>
+              <div className="project-description">Official website of Palmdrive</div>
+              {this._renderArrow()}
+            </div>
+          </Link>
         </div>
         <div className="project-section">
-          <div className="img-block">
-            <img className="background-cloud purple-cloud" src="svg/purplecloud.svg"/>
-            <img className="project-image" src="home/group-3/group-3@3x.png"/>
-          </div>
-          <div className="text-block">
-            <div className="project-title">Transaction Management Design</div>
-            <div className="project-description">Transaction Management is one of the five core functions of Pefin, a web-based financial tool</div>
-            <div className="view-case">
-              <Link to="/work/transaction" className="view-case-description">view case</Link>
-              <div className="view-case-arrow"></div>
-              <div className="view-case-arrow-head">></div>
+          <Link to="/work/transaction">
+            <div className="img-block">
+              <img className="background-cloud purple-cloud" src="svg/purplecloud.svg"/>
+              <img className="project-image" src="home/group-3/group-3@3x.png"/>
             </div>
-          </div>
+            <div className="text-block">
+              <div className="project-title">Transaction Management Design</div>
+              <div className="project-description">Transaction Management is one of the five core functions of Pefin, a web-based financial tool</div>
+              {this._renderArrow()}
+            </div>
+          </Link>
         </div>
         <div className="project-section">
-          <div className="img-block" src={logo}>
-            <img className="background-cloud" src="svg/pinkcloud.svg"/>
-            <div className="graphic-label">2013-present</div>
-          </div>
-          <div className="text-block">
-            <div className="project-title">Graphic Design Collection +</div>
-            <div className="project-description">A showcase of some other projects I worked on and shipped from 2013 till now</div>
-            <div className="view-case">
-              <Link to="/work/graphic" className="view-case-description">view case</Link>
-              <div className="view-case-arrow"></div>
-              <div className="view-case-arrow-head">></div>
+          <Link to="/work/graphic">
+            <div className="img-block" src={logo}>
+              <img className="background-cloud" src="svg/pinkcloud.svg"/>
+              <div className="graphic-label">2013-present</div>
             </div>
-          </div>
+            <div className="text-block">
+              <div className="project-title">Graphic Design Collection +</div>
+              <div className="project-description">A showcase of some other projects I worked on and shipped from 2013 till now</div>
+              {this._renderArrow()}
+            </div>
+          </Link>
         </div>
       </div>
     );
